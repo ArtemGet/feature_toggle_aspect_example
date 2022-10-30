ed2cba1b9de77d95d0d9fc0c839dfd0dd56e2eb7
Artem Getmanskii <re910@yandex.ru>
2022-10-30 17:35:08 +0300

    init

---
+++ src/test/java/artemget/example/feature_toggle/FeatureToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package artemget.example.feature_toggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
